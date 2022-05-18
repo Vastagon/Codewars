65e0e030c470d07aab3b4778510ae0ff434d6c4c
Vastagon <77780118+Vastagon@users.noreply.github.com>
2022-05-18 02:44:29 -0500

    Create Reverse Linked list.js

---
+++ leetcode/Reverse Linked list.js
@@ -0,0 +1,11 @@
+var reverseList = function(head) {
+    let prev = null
+    
+    while(head){
+        let next = head.next
+        head.next = prev
+        prev = head
+        head = next
+    }
+    return prev
+};
