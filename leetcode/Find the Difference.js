c9a65b71f526a31e55fdf927c58c00d433437fdb
Vastagon <77780118+Vastagon@users.noreply.github.com>
2022-05-24 04:50:37 -0500

    Create Find the Difference.js

---
+++ leetcode/Find the Difference.js
@@ -0,0 +1,5 @@
+var findTheDifference = function(s, t) {
+  const sum1 = s.split('').reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  const sum2 = t.split('').reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  return String.fromCharCode(sum2 - sum1);
+};
