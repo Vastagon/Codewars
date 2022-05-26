3ca7634e8316b133703b474452e5c1ac8e056409
Vastagon <77780118+Vastagon@users.noreply.github.com>
2022-05-26 04:32:32 -0500

    Update Excel Sheet Column Number.js

--- leetcode/Excel Sheet Column Number.js
+++ leetcode/Excel Sheet Column Number.js
@@ -3,16 +3,10 @@ var titleToNumber = function(columnTitle) {
     let sum = 0
 
     for(let i = columnTitle.length; i > 0 ; i--){
-        if(counter > 0){
-            sum = sum + ((columnTitle.charCodeAt(i-1) - 64) * (26 ** counter))
-        }else{
-            sum = sum + ((columnTitle.charCodeAt(i-1) - 64))
-        }
-        
+        sum = sum + ((columnTitle.charCodeAt(i-1) - 64) * (26 ** counter))
+                
         counter++
     }
 
     return sum
 };
-
-
