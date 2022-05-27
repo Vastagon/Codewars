b8ec5f90d8a708aa811311d71d7ac91ab121d7f5
Vastagon <77780118+Vastagon@users.noreply.github.com>
2022-05-27 00:16:07 -0500

    Create Move Zeroes.js

---
+++ leetcode/Move Zeroes.js
@@ -0,0 +1,13 @@
+var moveZeroes = function(nums) {
+    let len = nums.length
+    
+    for(let i = 0; i < len; i++){
+        if(nums[i] === 0){
+            nums.splice(i,1)
+            nums.push(0)
+            len--
+            i--
+        }
+    }
+    return nums
+};
