1d2f468ea77a46dfbf0a918c0e89209b6883e0cc
Vastagon <77780118+Vastagon@users.noreply.github.com>
2022-06-23 07:02:22 -0500

    Create Rotate Array.js

---
+++ leetcode/Rotate Array.js
@@ -0,0 +1,17 @@
+var rotate = function(nums, k) {
+   k %= nums.length // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
+    
+   let reverse = function(i, j){
+    while(i < j){
+        let temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+        i++
+        j--
+    }
+   } // suppose  ----->---> 
+	reverse(0, nums.length-1); // reverse   <--<------
+	 reverse(0, k-1) // reverse first part ---><----
+   reverse(k, nums.length-1)// reverse second part --->----->
+    
+};
