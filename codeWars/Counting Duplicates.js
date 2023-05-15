6e6320939fa06709e99d721d7e54d1da4dbdc430
Vastagon <77780118+Vastagon@users.noreply.github.com>
2023-05-14 21:03:25 -0500

    Create Counting Duplicates.js

---
+++ codeWars/Counting Duplicates.js
@@ -0,0 +1,14 @@
+function duplicateCount(text){
+  let lower = text.toLowerCase();
+  let count = 0;
+  let used = [];
+  
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+  
+  return count;
+}
