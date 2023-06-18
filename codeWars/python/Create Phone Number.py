0a9b4794ff0d7f8060466bfced4524b8cd6aafa6
Vastagon <77780118+Vastagon@users.noreply.github.com>
2023-06-17 23:47:04 -0500

    Create Create Phone Number.py

---
+++ codeWars/python/Create Phone Number.py
@@ -0,0 +1,6 @@
+def create_phone_number(n):
+    destructuredArray = "".join("{0}".format(i) for i in n)
+    
+    formattedString = "(" + destructuredArray[0:3] + ") " + destructuredArray[3:6] + "-" + destructuredArray[6:10]
+
+    return formattedString
